4e03e790f428d773079fe17c768c4e73ecd272f6
qlwong <730402725@qq.com>
2019-03-30 16:30:41 +0800

    主页布局

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,15 +1,52 @@
 <template>
-    <div>
-        
-    首页
-    </div>
+  <el-container class="index-container">
+    <el-header class="my_header">
+      <el-row>
+        <el-col :span="4">
+         <img src="../assets/index.png" alt="">
+        </el-col>
+        <el-col :span="19">
+         <h2>后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <el-button type="danger" round>退出</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container class="my_container">
+      <el-aside width="200px" class="my_aside">Aside</el-aside>
+      <el-main class="my_main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
-   export default {
-    name:'index',
-}
+export default {
+  name: "index"
+};
 </script>
-<style lang="">
-   
-    
+<style lang="scss">
+ .index-container{
+     height: 100%;
+     display: flex;
+     flex-direction: column;
+     .my_header{
+         height: 60px;
+         background-color: #b3c0d1;
+         line-height: 60px;
+         h2{
+             margin: 0;
+             padding: 0;
+             text-align: center;
+         }
+     }
+     .my_container{
+         flex:1;
+         .my_aside{
+             background-color: yellowgreen;
+         }
+         .my_main{
+             background-color: skyblue;
+         }
+     }
+ }
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,45 +1,84 @@
 <template>
-    <div class="login">
-       <div class="box">
-           <el-form label-position="top" label-width="80px" >
-       <el-form-item label="用户名">
-     <el-input ></el-input>
-     </el-form-item>
-    <el-form-item label="密码">
-    <el-input ></el-input>
-     </el-form-item>
-    <el-form-item>
-    <el-button type="primary" class="my_btn">登录</el-button>
-    
-  </el-form-item>
-</el-form>
-       </div>
-
+  <div class="login">
+    <div class="box">
+      <h2>用户登录</h2>
+      <el-form
+        label-position="top"
+        label-width="80px"
+        :model="loginForm"
+        :rules="rules"
+        ref="loginForm"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submit('loginForm')">登录</el-button>
+          <el-button type="success" @click="resetForm('loginForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 <script>
-  export default {
-    name:'login',
-}
+export default {
+  name: "login",
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 4, max: 16, message: "长度在 4 到 16 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 16, message: "长度在 6 到 16 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submit(formName) {
+      this.$refs[formName].validate(valid=>{
+        if(valid){
+
+        }else{
+          this.$message.error('数据类型错误,根据提示重写');
+          return false;
+        }
+      })
+    },
+    resetForm(formName){
+      this.$refs[formName].resetFields();
+    }
+  }
+};
 </script>
 <style lang="scss">
-    .login{
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: #324152;
-            .box{
-                width: 550px;
-                height: 420px;
-                border-radius: 10px;
-                /* 怪异盒模型 */
-                box-sizing: border-box;
-                padding: 35px;
-                background-color: #fff;
-                .my_btn{
-                width: 100%;
-            }
-            }
-        }
+.login {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #324152;
+  .box {
+    width: 550px;
+    height: 420px;
+    border-radius: 10px;
+    /* 怪异盒模型 */
+    box-sizing: border-box;
+    padding: 35px;
+    background-color: #fff;
+    .my_btn {
+      width: 100%;
+    }
+  }
+}
 </style>
